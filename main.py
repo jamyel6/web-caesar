c9bd4816bdfbfb2991507373d98cf9508c786d07
hbiggs <hbiggs>
2017-01-25 23:50:47 -0600

    final

--- main.py
+++ main.py
@@ -16,12 +16,37 @@
 #
 import webapp2
 import caesar
+import cgi
+
+def build_page(textarea_content):
+    rot_label = "<label>Rotate by:</label>"
+    rotation_input = "<input type='number' name='rotation'/>"
+
+    message_label = "<label>Type a message:</label>"
+    textarea = "<textarea name='message'>" +textarea_content + "</textarea>"
+
+    submit = "<input type= 'submit'/>"
+    form = ("<form method ='post'>" +
+    rot_label + rotation_input +  "<br>" +
+    message_label + textarea + "<br>"
+    + submit + "</form>")
+
+    header = "<h2> Web Caesar</h2>"
+
+    return header + form
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = 'Hello World!'
-        encrpted_message = caesar.rotate_character(message, 13)
+        content = build_page("")
+        self.response.write(content)
 
-        self.response.write(encrpted_message)
+    def post(self):
+        message = self.request.get("message")
+        rotation = int(self.request.get("rotation"))
+        encrpted_message = caesar.rotate_character(message, rotation)
+        escaped_messsage = cgi.escape(encrpted_message)
+        content = build_page(escaped_messsage)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
